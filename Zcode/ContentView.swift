220ee8c7e634b01bb1bc54a17c2bfd3d83e004e5
Samara <97859147+ssalggnikool@users.noreply.github.com>
2024-01-23 19:20:18 -0800

    Initial Commit

---
+++ Zcode/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zcode
+//
+//  Created by samara on 1/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zcode/ZcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZcodeApp.swift
+//  Zcode
+//
+//  Created by samara on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct ZcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
